28b70d207651feb1d53a32d24ed971cdfc7a7ad0
lipeisheng <1245441817@qq.com>
2018-12-20 12:28:01 +0800

    启动页

---
+++ AndroidClient/app/src/main/java/com/chat/androidclient/mvvm/view/activity/SplashAvtivity.kt
@@ -0,0 +1,13 @@
+package com.chat.androidclient.mvvm.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.chat.androidclient.R
+
+class SplashAvtivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_avtivity)
+    }
+}
