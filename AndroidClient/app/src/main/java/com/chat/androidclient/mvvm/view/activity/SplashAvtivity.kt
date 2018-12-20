19fce80a89720e522996857a9eb06717c23e8de5
lipeisheng <1245441817@qq.com>
2018-12-20 17:05:03 +0800

    更换图标

--- AndroidClient/app/src/main/java/com/chat/androidclient/mvvm/view/activity/SplashAvtivity.kt
+++ AndroidClient/app/src/main/java/com/chat/androidclient/mvvm/view/activity/SplashAvtivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.blankj.utilcode.util.SPUtils
 import com.chat.androidclient.R
 
 class SplashAvtivity : AppCompatActivity() {
+    companion object {
+        val LOGINSTATE="loginstate"
+    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_avtivity)
         Handler().postDelayed({
-            startActivity(Intent(this,LoginActivity::class.java))
+            val loginstate = SPUtils.getInstance().getBoolean(LOGINSTATE)
+            if (loginstate){
+                startActivity(Intent(this,MainActivity::class.java))
+            }else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+            finish()
         },500)
     }
 }
