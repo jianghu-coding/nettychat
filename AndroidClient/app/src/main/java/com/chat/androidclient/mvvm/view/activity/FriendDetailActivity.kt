950552d8b6ac88fe0b6d38e866f831e597a5a0de
lipeisheng <1245441817@qq.com>
2019-01-08 18:01:02 +0800

    profile ui

--- AndroidClient/app/src/main/java/com/chat/androidclient/mvvm/view/activity/FriendDetailActivity.kt
+++ AndroidClient/app/src/main/java/com/chat/androidclient/mvvm/view/activity/FriendDetailActivity.kt
@@ -1,5 +1,12 @@
 package com.chat.androidclient.mvvm.view.activity
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.view.WindowManager
+import com.blankj.utilcode.util.BarUtils
+import com.blankj.utilcode.util.LogUtils
+import com.blankj.utilcode.util.SpanUtils
 import com.chat.androidclient.R
 import com.chat.androidclient.databinding.ActivityFriendDetailBinding
 import com.chat.androidclient.mvvm.viewmodel.FriendDetailVM
@@ -8,5 +15,22 @@ class FriendDetailActivity : BaseActivity<ActivityFriendDetailBinding,FriendDeta
     override fun getViewModel()= FriendDetailVM (this)
     override fun getLayoutRes()=R.layout.activity_friend_detail
     
- 
+    override fun init() {
+        super.init()
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+//        BarUtils.setStatusBarColor(this,Color.TRANSPARENT)
+//        BarUtils.setStatusBarAlpha(this,15)
+        mDataBinding.appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            LogUtils.e(""+verticalOffset)
+//            LogUtils.e(""+mDataBinding.appBar.totalScrollRange)
+            if (Math.abs(verticalOffset)>=mDataBinding.appBar.totalScrollRange){
+//                mDataBinding.collapslayout.contentScrim=ColorDrawable(Color.parseColor("#41c5f6"))
+//            mDataBinding.titleLayout.setBackgroundColor(resources.getColor(R.color.color41c5f6))
+            }else{
+//                mDataBinding.titleLayout.setBackgroundColor(0)
+    
+            }
+        }
+    }
 }
